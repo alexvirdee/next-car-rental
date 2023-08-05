e13738812d827bd820c42a8e384c8746d21a13e9
Alex Virdee <alex.virdee08@gmail.com>
2023-08-05 17:34:47 -0400

    login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,51 @@
+'use client'
 import React from 'react'
+import { Form, Button } from 'antd'
+import Link from 'next/link'
+import { antdFieldValidation } from '@/helpers/validationHelpers'
 
 function Login() {
+
+  const onFinish = (values: any) => {
+    console.log('values', values)
+  }
+
   return (
-    <div>Login</div>
+    <div className='flex justify-center items-center h-screen'>
+      <div className="w-450 card p-5">
+        <Form
+          layout="vertical"
+          onFinish={onFinish}
+        >
+          <h1 className="text-xl">RideRoster - Login</h1>
+
+          <div className="divider"></div>
+
+          <div className="flex flex-col gap-5">
+
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={antdFieldValidation}
+            >
+              <input type="email" />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={antdFieldValidation}
+            >
+              <input type="password" />
+            </Form.Item>
+
+            <Button type="primary" block htmlType="submit">Login</Button>
+
+            <Link href="/register">Don't have an account? Register</Link>
+          </div>
+        </Form>
+      </div>
+    </div>
   )
 }
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,35 +2,54 @@
 import React from 'react'
 import { Form, Button } from 'antd'
 import Link from 'next/link'
+import { antdFieldValidation } from '@/helpers/validationHelpers'
 
 function Register() {
+
+  const onFinish = (values: any) => {
+    console.log('values', values)
+  }
+
   return (
     <div className='flex justify-center items-center h-screen'>
       <div className="w-450 card p-5">
         <Form
           layout="vertical"
+          onFinish={onFinish}
         >
           <h1 className="text-xl">RideRoster - Register</h1>
 
           <div className="divider"></div>
 
-         <div className="flex flex-col gap-5">
-         <Form.Item label="Name" name="name">
-            <input type="text" />
-          </Form.Item>
+          <div className="flex flex-col gap-5">
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={antdFieldValidation}
+            >
+              <input type="text" />
+            </Form.Item>
 
-          <Form.Item label="Email" name="email">
-            <input type="email" />
-          </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={antdFieldValidation}
+            >
+              <input type="email" />
+            </Form.Item>
 
-          <Form.Item label="Password" name="password">
-            <input type="password" />
-          </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={antdFieldValidation}
+            >
+              <input type="password" />
+            </Form.Item>
 
-          <Button type="primary">Register</Button>
+            <Button type="primary" block htmlType="submit">Register</Button>
 
-          <Link href="/login">Already have an account? Login</Link>
-         </div>
+            <Link href="/login">Already have an account? Login</Link>
+          </div>
         </Form>
       </div>
     </div>
